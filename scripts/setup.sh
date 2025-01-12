f0b9a7224289776bd64273671feec18049461b21
czarkos <chrisarkos16@gmail.com>
2025-01-12 01:48:12 -0800

    Moved scripts to script directory

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 # get script working path
-current_dir=$(pwd)
+script_run_dir=$(pwd)
 
 echo "---------------------------------------------------------"
 echo "-------------------SETTING UP PROTOBUF-------------------"
@@ -17,8 +19,8 @@ echo "---------------------------------------------------------"
 cd qpl
 mkdir build
 cd build
-read -p "Enter the (full) path for the qpl installation directory [Default: script_path_dir/qpl_install_dir]: " qpl_install_dir
-default_dir="${current_dir}/qpl_install_dir"
+read -p "Enter the (full) path for the qpl installation directory [Default: script_run_path_dir/qpl_install_dir]: " qpl_install_dir
+default_dir="${script_run_dir}/qpl_install_dir"
 qpl_install_dir=${qpl_install_dir:-$default_dir}
 echo "Your installation dir is $qpl_install_dir."
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$qpl_install_dir ..
@@ -26,6 +28,3 @@ cmake --build . --target install -j16
 echo "---------------------------------------------------------"
 echo "---------------------QPL BUILD OVER----------------------"
 echo "---------------------------------------------------------"
-
-# make sure the environmental variable for protobuf is correct
-export PROTO_PATH="${current_dir}/protobuf"
